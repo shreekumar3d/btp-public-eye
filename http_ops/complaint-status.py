189234315bb16576b4ee382eb8fed35a003bae8e
Shree Kumar <shkumar@innominds.com>
2014-01-21 23:15:28 +0530

    _Use_ complaint no. Include rejection reason.
    Example : complaint 6765

--- http_ops/complaint-status.py
+++ http_ops/complaint-status.py
@@ -28,7 +28,7 @@ viewstate = soup.select("#__VIEWSTATE")[0]['value']
 eventvalidation = soup.select("#__EVENTVALIDATION")[0]['value']
 
 # Now, fire the real POST request that will return the data
-values = {'txtCompno': '6891', 'btnSubmit':'Submit' }
+values = {'txtCompno': complaintNumber, 'btnSubmit':'Submit' }
 values['__VIEWSTATE']=viewstate
 values['__EVENTVALIDATION']=eventvalidation
 r = requests.post(url, data=values)
@@ -42,14 +42,16 @@ r = requests.post(url, data=values)
 #print soup.select('#divVdateTm')
 #
 soup = BeautifulSoup(r.text)
-attribs = [ ("Reg No   ", 'Regno'), 
-            ("Violation", 'Vtype'), 
-            ("Date     ", 'Rdate'),
-            ("Time     ", 'VdateTm'),
-            ("Remarks  ", 'Premarks'), 
-            ("Status   ", 'BS2'), 
+attribs = [ ("Reg No           ", 'Regno'), 
+            ("Violation        ", 'Vtype'), 
+            ("Date             ", 'Rdate'),
+            ("Time             ", 'VdateTm'),
+            ("Remarks          ", 'Premarks'), 
+            ("Status           ", 'BS2'), 
+            ("Rejection Reason ", 'BRejReason'), 
           ]
 for attr in attribs:
 	n = soup.select('#div%s'%(attr[1]))
 	val = n[0].text
 	print "%s: %s"%(attr[0], val)
+
